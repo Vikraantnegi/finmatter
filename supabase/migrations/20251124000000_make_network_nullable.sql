9a53a689a4552bd07b376a3a839ec145499b9a35
Vikrant negi <vikrant.negi74@gmail.com>
2025-11-24 21:42:26 +0530

    feat: remove migrations

--- supabase/migrations/20251124000000_make_network_nullable.sql
+++
@@ -1,7 +0,0 @@
--- Migration: Make network column nullable in cards table
--- Date: 2025-11-24
--- Description: Allow null network when BIN lookup fails
-
--- Make network column nullable
-ALTER TABLE cards ALTER COLUMN network DROP NOT NULL;
-
